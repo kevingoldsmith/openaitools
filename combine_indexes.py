fee0d1030343a70034852da638f7c10524c8ca71
Kevin Goldsmith <kevin@unitcircle.com>
2023-08-29 14:47:50 -0700

    checkpoint before I update combine indexes

--- combine_indexes.py
+++ combine_indexes.py
@@ -11,8 +11,9 @@ __OUTPUT_DIR = 'output'
 def fix_index(index_to_fix:dict)->dict:
     print("fixing index")
     output = {}
+    first_index = next(iter(index_to_fix))
 
-    if next(iter(index_to_fix)) == "Index":
+    if first_index == "Index":
         terms = index_to_fix['Index']
         count = 1
         for term in terms:
@@ -26,20 +27,51 @@ def fix_index(index_to_fix:dict)->dict:
         count += 1 
         return output
 
-    if next(iter(index_to_fix)) == "index" and isinstance(index_to_fix['index'], list):
+    if first_index == "index" and isinstance(index_to_fix[first_index], list):
         count = 1
         for term in index_part['index']:
             output[str(count)] = term
             count += 1
         return output
+    
+    if first_index == "index" and isinstance(index_to_fix[first_index], dict):
+        return index_to_fix[first_index]
 
-    if next(iter(index_to_fix)) == "Document" and isinstance(index_to_fix['Document'], dict):
+    if first_index == "Document" and isinstance(index_to_fix[first_index], dict):
         # need to unroll it, pull the keywords out, add section titles as context, then add related terms to each
         print("document style")
+    
+    if isinstance(index_to_fix[first_index], dict) and "Related Terms" in index_to_fix[first_index]:
+        # need to turn each dictionary item into the proper format, extract anything in the proper names list into their own index items
+        print("dict style")
 
-    print("failed to fix!")
+    print("FAILED TO FIX!")
     return index_to_fix
 
+
+def is_valid_index_json(index_part:dict)->bool:
+    for key in index_part:
+        if not key.isdigit():
+            return False
+        if not int(key) in range(1,50):
+            return False
+        value = index_part[key]
+        if not isinstance(value, dict):
+            return False
+        if not "term" in value:
+            return False
+        if not isinstance(value['term'], str):
+            return False
+        if not "related_terms" in value:
+            return False
+        if not isinstance(value['related_terms'], list):
+            return False
+#        if not "context" in value:
+#            return False
+#        if not isinstance(value['context'], list):
+#            return False
+        # don't care about other stuff at the moment
+    return True
 
 files_to_parse = os.listdir(__OUTPUT_DIR)
 master_index = {}
@@ -50,7 +82,9 @@ for index_file in sorted(files_to_parse):
     base_file_name = os.path.splitext(index_file)[0]
     with open(os.path.join(__OUTPUT_DIR, index_file)) as f:
         index_part = json.load(f)
-        # expected format is something like: { "1": { "term": "word", "related_terms": [], "context": [], "frequency": <string or number>}, ... }
+        # expected format is: { "1": { "term": "word", "related_terms": [], "context": [], "frequency": <string or number>}, ... }
+        # context may be missing
         # reformat the json if needed
         if next(iter(index_part)) != "1":
             index_part = fix_index(index_part)
+        print(f"valid index: {is_valid_index_json(index_part)}")
--- create_index_files.py
+++ create_index_files.py
@@ -9,15 +9,26 @@ __CONTENT_DIR = 'content'
 __OUTPUT_DIR = 'output'
 __CONTENT_PROMPT_LENGTH=200
 
-prompt = """Using the following criteria for selection, provide a JSON-formatted structured index of the following chunk of text, ignoring Markdown syntax like bullet points and links but considering headers for context. and limit the length of the index to 25 terms:
+prompt = """Using the following criteria for selection, provide a JSON-formatted structured index of the following chunk of text, ignoring Markdown syntax like bullet points and links but considering headers for context. Limit the length of the index to 25 terms:
 a. Relevance to leadership of software development organizations
-b. Keywords include agile, management, software architecture and engineering
-c. Word is used frequently
-d. Term is specific and precise
-e. Suggest related terms for cross-referencing
-f. Accommodate professional-level reader comprehension
-g. Consistent, organized and comprehensive results
-h. Proper Names
+b. Word is used frequently
+c. Term is specific and precise
+d. Suggest related terms for cross-referencing
+e. Accommodate professional-level reader comprehension
+f. Consistent, organized and comprehensive results
+g. Proper Names
+
+the format of the JSON should be of the form
+{
+     "1": {
+                 "term": <term>,
+                 "related_terms": [],
+                 "context": []
+      },
+      ...
+}
+
+The text to be indexed is:
 """
 
 config_parser = configparser.ConfigParser()
@@ -27,63 +38,66 @@ openai.api_key = openai_secret
 files_to_parse = os.listdir(__CONTENT_DIR)
 
 for mdfile in sorted(files_to_parse):
-  if not mdfile.endswith('.md'):
-    continue
-  print(mdfile)
-  base_file_name = os.path.splitext(mdfile)[0]
-  
-  sections = []
-  current_section = { 'title': '', 'contents': [] }
-  with open(os.path.join(__CONTENT_DIR, mdfile)) as f:
-    for line in f:
-      if not line.startswith('# '):
-        if line != '\n':
-          current_section['contents'].append(line)
-      else:
-        sections.append(current_section)
-        current_section = {'title': line[2:], 'contents': []}
-    sections.append(current_section)
+	if not mdfile.endswith('.md'):
+		continue
+	print(mdfile)
+	base_file_name = os.path.splitext(mdfile)[0]
 
-  section_number = 1
-  for section in sections:
-    if (len(section['contents'])) < 5:
-      continue
-    print(f"{section['title']}: {len(section['contents'])} lines")
-    section_parts = [section['contents'][i * __CONTENT_PROMPT_LENGTH:(i + 1) * __CONTENT_PROMPT_LENGTH] for i in range((len(section['contents']) + __CONTENT_PROMPT_LENGTH - 1) // __CONTENT_PROMPT_LENGTH )] 
-    print(f"parts: {len(section_parts)}")
+	sections = []
+	current_section = { 'title': '', 'contents': [] }
+	with open(os.path.join(__CONTENT_DIR, mdfile)) as f:
+		for line in f:
+			if not line.startswith('# '):
+				if line != '\n':
+					current_section['contents'].append(line)
+			else:
+				sections.append(current_section)
+				current_section = {'title': line[2:], 'contents': []}
+		sections.append(current_section)
 
-    lines = 0
-    for part in section_parts:
-      print('calling OpenAI')
-      text = ''.join(part)
-      response = openai.ChatCompletion.create(
-        model="gpt-4",
-        messages=[
-          {
-            "role": "user",
-            "content": prompt+text
-          }
-        ],
-        temperature=0.5,
-        max_tokens=4096,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-      )
+	section_number = 1
+	for section in sections:
+		if (len(section['contents'])) < 5:
+			continue
+		print(f"{section['title']}: {len(section['contents'])} lines")
+		section_parts = [section['contents'][i * __CONTENT_PROMPT_LENGTH:(i + 1) * __CONTENT_PROMPT_LENGTH] for i in range((len(section['contents']) + __CONTENT_PROMPT_LENGTH - 1) // __CONTENT_PROMPT_LENGTH )] 
+		print(f"parts: {len(section_parts)}")
 
-      print('writing files')
-      if not os.path.exists(__OUTPUT_DIR):
-        os.mkdir(__OUTPUT_DIR)
-      outfile = os.path.join(__OUTPUT_DIR, f"{base_file_name}-{section_number:02d}-{''.join(x for x in section['title'] if x.isalnum())}-{lines:04d}-{(lines+len(part)):04d}")
-      with open(outfile+'.txt', 'w') as f:
-        f.write(json.dumps(response, indent=2))
+		lines = 0
+		for part in section_parts:
+			if not os.path.exists(__OUTPUT_DIR):
+				os.mkdir(__OUTPUT_DIR)
+			outfile = os.path.join(__OUTPUT_DIR, f"{base_file_name}-{section_number:02d}-{''.join(x for x in section['title'] if x.isalnum())}-{lines:04d}-{(lines+len(part)):04d}")
+			if not os.path.exists(outfile+'.json'):
+				print('calling OpenAI')
+				text = ''.join(part)
+				response = openai.ChatCompletion.create(
+					model="gpt-4",
+					messages=[
+					{
+						"role": "user",
+						"content": prompt+text
+					}
+					],
+					temperature=0.5,
+					max_tokens=4096,
+					top_p=1,
+					frequency_penalty=0,
+					presence_penalty=0
+				)
 
-      with open(outfile+'.json', 'w') as f:
-        result = response['choices'][0]['message']['content']
-        result_json = json.loads(result)
-        f.write(json.dumps(result_json, indent=2))
+				print('writing files')
+				with open(outfile+'.txt', 'w') as f:
+					f.write(json.dumps(response, indent=2))
 
-      lines+=len(part)
-      section_number += 1
+				with open(outfile+'.json', 'w') as f:
+					result = response['choices'][0]['message']['content']
+					result_json = json.loads(result)
+					f.write(json.dumps(result_json, indent=2))
+			else:
+				print("output file exists, skipping")
 
-  print('\n')
+			lines+=len(part)
+			section_number += 1
+
+	print('\n')
